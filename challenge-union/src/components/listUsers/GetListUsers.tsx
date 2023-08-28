f2da6a919282bf6df0452003fbb4c77131f7d448
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2023-08-28 16:03:42 -0300

    Feat: Create Component Show List Users

---
+++ challenge-union/src/components/listUsers/GetListUsers.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+import { UserType } from '../../types/UserType';
+import { TableUsers } from './TableUsers';
+const QTDE_USERS = 10;
+const URL = `https://randomuser.me/api/?results=${QTDE_USERS}`;
+
+export const GetListUsers = () => {
+	const [users, setUsers] = useState<UserType[]>([]);
+	async function GetData() {
+		const response = await fetch(URL);
+		const listUsers = await response.json();
+		setUsers(listUsers.results);
+	}
+	useEffect(() => {
+		GetData();
+	}, []);
+	return <section>{users.length ? <TableUsers users={users} /> : <p>Carregando ...</p>}</section>;
+};

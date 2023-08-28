71964e59180c37b39c85a9dd7176fba9aefc3900
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2023-08-28 18:33:16 -0300

    Refactor: Add Semantic Tags

--- challenge-union/src/components/listUsers/TableUsers.tsx
+++ challenge-union/src/components/listUsers/TableUsers.tsx
@@ -5,28 +5,32 @@ import { GenerateIdUser } from '../../utils/GenerateIdUser';
 export const TableUsers = ({ users }: TableUserType) => {
 	return (
 		<table>
-			<tr>
-				<th>ID</th>
-				<th>First Name</th>
-				<th>Last Name</th>
-				<th>Title</th>
-				<th>Date</th>
-				<th>Age</th>
-				<th>Actions</th>
-			</tr>
-			{users.map((user, index) => (
-				<tr key={index}>
-					<td>{GenerateIdUser(user.login.uuid)}</td>
-					<td>{user.name.first}</td>
-					<td>{user.name.last}</td>
-					<td>{user.name.title}</td>
-					<td>{FormattedDate(user.dob.date)}</td>
-					<td>{user.dob.age}</td>
-					<td>
-						<button>View profile</button>
-					</td>
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>Title</th>
+					<th>Date</th>
+					<th>Age</th>
+					<th>Actions</th>
 				</tr>
-			))}
+			</thead>
+			<tbody>
+				{users.map((user, index) => (
+					<tr key={index}>
+						<td>{GenerateIdUser(user.login.uuid)}</td>
+						<td>{user.name.first}</td>
+						<td>{user.name.last}</td>
+						<td>{user.name.title}</td>
+						<td>{FormattedDate(user.dob.date)}</td>
+						<td>{user.dob.age}</td>
+						<td>
+							<button>View profile</button>
+						</td>
+					</tr>
+				))}
+			</tbody>
 		</table>
 	);
 };

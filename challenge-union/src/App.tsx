a3ae8204d9537a60e728bb5e2c6b6fc98db2ca3c
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2023-08-28 12:03:42 -0300

    Feat: Add Outlet

--- challenge-union/src/App.tsx
+++ challenge-union/src/App.tsx
@@ -1,9 +1,12 @@
+import { Outlet } from 'react-router';
+
 function App() {
-  return (
-    <>
-     <h1>Iniciando o Projeto</h1>
-    </>
-  )
+	return (
+		<>
+			<h1>Iniciando o Projeto</h1>
+			<Outlet />
+		</>
+	);
 }
 
-export default App
+export default App;

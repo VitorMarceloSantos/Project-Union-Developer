8096dd80928094378e31f9f66d6fe1057541baab
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2023-08-28 12:04:55 -0300

    Feat: Create Route - Index

--- challenge-union/src/main.tsx
+++ challenge-union/src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { GetListUsers } from './components/listUsers/GetListUsers.tsx';
 
+const router = createBrowserRouter([
+	{
+		element: <App />,
+		children: [
+			{
+				path: '/',
+				element: <GetListUsers />,
+			},
+			// {
+			// 	path: '/user-details',
+			// 	element: '',
+			// },
+		],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
+);

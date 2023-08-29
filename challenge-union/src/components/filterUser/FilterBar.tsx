db38dc7acdeef70cb0eaeba29cf3fac43701854e
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2023-08-29 16:16:09 -0300

    Feat: Create Component FilterUser

---
+++ challenge-union/src/components/filterUser/FilterBar.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { UserFilterType } from '../../types/FilterTypes';
+
+export const FilterBar = ({ filterProps }: UserFilterType) => {
+	const { usersPagination, setUsersFiltered } = filterProps;
+	const [userSearch, setUserSearch] = useState<string>('');
+
+	const HandlerFindUser = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		// A busca está senda realizado pelo First Name.
+		const { target } = event;
+		const targetInputLower = target.value.toLowerCase();
+		const filtered = usersPagination.filter(({ name }) => name.first.toLocaleLowerCase().includes(targetInputLower));
+		// Find: caso não encontre nenhum resultado será retornado undefined,
+		// Caso o input esteja vazio(target.value), será atribuido um array vazio [].
+		if (filtered.length !== undefined && target.value.length !== 0) {
+			setUsersFiltered(filtered);
+		} else {
+			setUsersFiltered([]);
+		}
+		setUserSearch(target.value);
+	};
+
+	return (
+		<section>
+			<input type='text' placeholder='Search user...' value={userSearch} onChange={(event) => HandlerFindUser(event)} />
+		</section>
+	);
+};

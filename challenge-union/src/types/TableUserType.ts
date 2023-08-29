52fca92b9d0e550f015484e156bd5fa517d40160
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2023-08-29 13:49:52 -0300

    Feat: Add New Types

--- challenge-union/src/types/TableUserType.ts
+++ challenge-union/src/types/TableUserType.ts
@@ -1,5 +1,22 @@
+import { Dispatch, SetStateAction } from 'react';
 import { UserType } from './UserType';
 
 export type TableUserType = {
 	users: UserType[];
 };
+
+export type UserStateType = {
+	users: UserType[];
+	setUsers: Dispatch<SetStateAction<UserType[]>>;
+};
+
+export type UserPaginationType = {
+	usersPagination: UserType[];
+	setUsersPagination: Dispatch<SetStateAction<UserType[]>>;
+};
+
+export type GetListUsersType = {
+	userState: UserStateType;
+	userPagination: UserPaginationType;
+};
+

9daf3b3ac2f4a00222b2322b624414468daa46cd
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2023-08-29 22:23:15 -0300

    Refactor: update name

---
+++ challenge-union/src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import { useContext, useState } from 'react';
+import { UserType } from '../types/UserType';
+import { GetListUsers } from '../components/listUsers/GetListUsers';
+import { PaginationMenu } from '../components/pagination/PaginationMenu';
+import { FilterUser } from '../components/filterUser/FilterUser';
+import { UsersContext } from '../context/UsersContext';
+
+export const Home = () => {
+	const { stateUsers, handlerSetUsers } = useContext(UsersContext);
+	const [usersPagination, setUsersPagination] = useState<UserType[]>([]);
+	const [usersFiltered, setUsersFiltered] = useState<UserType[]>([]);
+
+	return (
+		<main>
+			<FilterUser filterProps={{ usersPagination, setUsersFiltered }} />
+			<GetListUsers
+				userState={{ stateUsers, handlerSetUsers }}
+				userPagination={{ usersPagination, setUsersPagination }}
+				usersFiltered={usersFiltered}
+			/>
+			<PaginationMenu stateUsers={stateUsers} setUsersPagination={setUsersPagination} />
+		</main>
+	);
+};

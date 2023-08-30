f7dcff6e37d3650140aebee2175dbd9cd785ff8a
Vitor Marcelo <vitor.marcelo2009@hotmail.com>
2023-08-30 18:51:47 -0300

    Refactor: Add Current Page

--- challenge-union/src/context/PaginationNumbersContext.tsx
+++ challenge-union/src/context/PaginationNumbersContext.tsx
@@ -2,6 +2,9 @@ import { createContext } from 'react';
 import { PaginationNumbersContextType } from '../types/ContextType';
 
 export const PaginationNumbersContext = createContext<PaginationNumbersContextType>({
-	paginationNumbers: [],
-	handlerSetPaginationNumbers: () => [],
+	pagination: {
+		paginationNumbers: [],
+		currentPage: 0,
+	},
+	handlerSetPaginationNumbers: () => {}
 });

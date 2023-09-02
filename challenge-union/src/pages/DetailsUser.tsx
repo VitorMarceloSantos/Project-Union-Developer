a4ac54406f1cec7459a024832861a963d7a7a491
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2023-09-02 14:19:55 -0300

    Refactor: Update useLocation

--- challenge-union/src/pages/DetailsUser.tsx
+++ challenge-union/src/pages/DetailsUser.tsx
@@ -2,8 +2,8 @@ import { useLocation, NavLink } from 'react-router-dom';
 import { UserType } from '../types/UserType';
 
 export const DetailsUser = () => {
-	const location = useLocation();
-	const state = location.state as UserType;
+	const { state } = useLocation();
+	const userDetail = state.user as UserType;
 	// Realizando a tipagem do state para receber os tipos de User.
 
 	return (
@@ -15,10 +15,10 @@ export const DetailsUser = () => {
 			</div>
 			<article className='details-user-profile'>
 				<div className='details-user-profile-image'>
-					<img src={state.picture.large} alt='My Photo' />
+					<img src={userDetail.picture.large} alt='My Photo' />
 				</div>
-				<h2 className='details-user-profile-title text-color-1'>{`${state.name.first} ${state.name.last}`}</h2>
-				<p className='text-color-2'>{state.name.title}</p>
+				<h2 className='details-user-profile-title text-color-1'>{`${userDetail.name.first} ${userDetail.name.last}`}</h2>
+				<p className='text-color-2'>{userDetail.name.title}</p>
 			</article>
 			<hr className='details-user-line' />
 			<section className='details-user-informations'>
@@ -27,24 +27,24 @@ export const DetailsUser = () => {
 					<div className='details-user-informations-generic-display'>
 						<div className='details-user-informations-generic-display-unit'>
 							<p className='text-color-1'>Firt Name</p>
-							<p className='text-color-2'>{state.name.first}</p>
+							<p className='text-color-2'>{userDetail.name.first}</p>
 						</div>
 						<div className='details-user-informations-generic-display-unit'>
 							<p className='text-color-1'>Last Name</p>
-							<p className='text-color-2'>{state.name.last}</p>
+							<p className='text-color-2'>{userDetail.name.last}</p>
 						</div>
 						<div className='details-user-informations-generic-display-unit'>
 							<p className='text-color-1'>Gender</p>
-							<p className='text-color-2'>{state.gender}</p>
+							<p className='text-color-2'>{userDetail.gender}</p>
 						</div>
 						<div className='details-user-informations-generic-display-unit'>
 							<p className='text-color-1'>Age</p>
-							<p className='text-color-2'>{state.dob.age}</p>
+							<p className='text-color-2'>{userDetail.dob.age}</p>
 						</div>
 						<div className='details-user-informations-generic-display-unit'>
 							<p className='text-color-1'>Phone</p>
-							<p className='text-color-2'>{state.phone}</p>
-							<p className='text-color-2'>{state.cell}</p>
+							<p className='text-color-2'>{userDetail.phone}</p>
+							<p className='text-color-2'>{userDetail.cell}</p>
 						</div>
 					</div>
 				</div>
@@ -53,23 +53,23 @@ export const DetailsUser = () => {
 					<div className='details-user-informations-generic-display'>
 						<div className='details-user-informations-generic-display-unit'>
 							<p className='text-color-1'>Street</p>
-							<p className='text-color-2'>{`${state.location.street.name} - ${state.location.street.number}`}</p>
+							<p className='text-color-2'>{`${userDetail.location.street.name} - ${userDetail.location.street.number}`}</p>
 						</div>
 						<div className='details-user-informations-generic-display-unit'>
 							<p className='text-color-1'>City</p>
-							<p className='text-color-2'>{state.location.city}</p>
+							<p className='text-color-2'>{userDetail.location.city}</p>
 						</div>
 						<div className='details-user-informations-generic-display-unit'>
 							<p className='text-color-1'>State</p>
-							<p className='text-color-2'>{state.location.state}</p>
+							<p className='text-color-2'>{userDetail.location.state}</p>
 						</div>
 						<div className='details-user-informations-generic-display-unit'>
 							<p className='text-color-1'>Country</p>
-							<p className='text-color-2'>{state.location.country}</p>
+							<p className='text-color-2'>{userDetail.location.country}</p>
 						</div>
 						<div className='details-user-informations-generic-display-unit'>
 							<p className='text-color-1'>PostCode</p>
-							<p className='text-color-2'>{state.location.postcode}</p>
+							<p className='text-color-2'>{userDetail.location.postcode}</p>
 						</div>
 					</div>
 				</div>
@@ -78,15 +78,15 @@ export const DetailsUser = () => {
 					<div className='details-user-informations-generic-display'>
 						<div className='details-user-informations-generic-display-unit'>
 							<p className='text-color-1'>Username</p>
-							<p className='text-color-2'>{state.login.username}</p>
+							<p className='text-color-2'>{userDetail.login.username}</p>
 						</div>
 						<div className='details-user-informations-generic-display-unit'>
 							<p className='text-color-1'>Email</p>
-							<p className='text-color-2'>{state.email}</p>
+							<p className='text-color-2'>{userDetail.email}</p>
 						</div>
 						<div className='details-user-informations-generic-display-unit'>
 							<p className='text-color-1'>Password</p>
-							<p className='text-color-2'>{state.login.password}</p>
+							<p className='text-color-2'>{userDetail.login.password}</p>
 						</div>
 					</div>
 				</div>
